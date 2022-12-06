a413b8533d7dbf9c5cbea2c7b8c2572a7c5dd30c
WUPINYING <sheluckys4@gmail.com>
2022-12-06 23:29:06 +0800

    Add informative line

--- ProGitForProgrammers/ProGitForProgrammers/Program.cs
+++ ProGitForProgrammers/ProGitForProgrammers/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace ProGitForProgrammers
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            Console.WriteLine("I just added this in Visual Studle");
+        }
+    }
+}
